e73640e3e20ac9f80fda70b56373cd34e63fccd8
leo3065 <leo3065@gmail.com>
2024-09-05 17:01:50 +0800

    Move the item definition to its own file

---
+++ items-data.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var items_all = [
+    {id: 'kushala_daora', name: 'Kushala Daora', image: 'image/offical_thumbnail/Kushala_Daora.png'},
+    {id: 'seregios', name: 'Seregios', image: 'image/offical_thumbnail/Seregios.png'},
+    {id: 'valstrax', name: 'Valstrax', image: 'image/offical_thumbnail/Valstrax.png'}
+];
